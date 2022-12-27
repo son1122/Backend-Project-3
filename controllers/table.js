d4c4a410a14ff208dfc32f3f91bb62dee480932f
jordanbae <laphon.nala@bumail.net>
2022-12-27 18:19:24 +0700

    added get all table function to use for mapping

--- controllers/table.js
+++ controllers/table.js
@@ -1,7 +1,14 @@
-const testTable = (req, res) => {
-    res.json("test_table")
-}
+const Tables = require("../models").Table;
+const getAllTable = async (req, res) => {
+  try {
+    await Tables.findAll().then((tables) => {
+      res.json(tables);
+    });
+  } catch {
+    res.status(500).send({ message: "Tables not found" });
+  }
+};
 
 module.exports = {
-    testTable,
-}
+  getAllTable,
+};
