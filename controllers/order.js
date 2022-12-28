f97577dade3a7722b03540cb30b2c7b3cd6bbc59
jordanbae <laphon.nala@bumail.net>
2022-12-28 18:02:05 +0700

    added create order function

--- controllers/order.js
+++ controllers/order.js
@@ -1,9 +1,44 @@
-const Order = require("../models/").Order;
+const Order = require("../models").Order;
+const OrderDetail = require("../models").OrderDetail;
 
 const testOrder = (req, res) => {
-  res.json("test_order");
+  Order.findAll()
+    .then((item) => {
+      console.log(item);
+      res.json(item);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
+const createOrder = async (req, res) => {
+  // I set customer to null since we currently not supporting customerId
+  const { menuItems, table_number, customer_id, order_date, status } = req.body;
 
+  // Create new order which takes data from the req.body above.
+  const order = await Order.create({
+    table_number,
+    customer_id,
+    order_date,
+    status,
+  });
+
+  //Looping through the items received, (can use mapping in case using promises but i dont know how)
+  for (const menuItem of menuItems) {
+    if (menuItem.id) {
+      await OrderDetail.create({
+        order_id: order.id,
+        menu_item_id: menuItem.id,
+        quantity: menuItem.quantity,
+      });
+    }
+  }
+
+  res.json({
+    success: { message: "Added order successfully." },
+  });
+};
 module.exports = {
   testOrder,
+  createOrder,
 };
