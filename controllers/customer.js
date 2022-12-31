11e6f0e23b09515c6d7ca69a8a05078a09b15ea7
son1122 <wongsatorn.n@hotmail.com>
2022-12-31 21:30:24 +0700

    add

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,8 +1,108 @@
+const User = require('../models').Customer
+const bcrypt = require('bcryptjs')
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
 
+
+const signup = (req, res) => {
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return res.status(500).json(err)
+
+        bcrypt.hash(req.body.password, salt, (err, hashedPwd) => {
+            if (err) return res.status(500).json(err)
+            req.body.password = hashedPwd
+            console.log(req.body)
+            User.create(req.body)
+                .then(newUser => {
+                    const token = jwt.sign(
+                        {
+                            username: newUser.username,
+                            id: newUser.id
+                        },
+                        process.env.JWT_SECRET,
+                        {
+                            expiresIn: "30 days",
+                        }
+                    )
+
+                    // res.cookie("jwt", token)
+                    console.log("fin")
+                    res.status(200).json({status: "signUp"})
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.send(`error ${err}`)
+                })
+        })
+
+    })
+}
+
+
+
+const login = (req, res) => {
+    console.log(req.body)
+    try {
+        User.findOne({
+            where: {
+                username: req.body.username
+            }
+        })
+            .then(foundUser => {
+                if (foundUser) {
+                    bcrypt.compare(req.body.password, foundUser.password, (err, match) => {
+                        if (match) {
+                            const token = jwt.sign(
+                                {
+                                    username: foundUser.username,
+                                    id: foundUser.id
+                                },
+                                process.env.JWT_SECRET,
+                                {
+                                    expiresIn: "30 days",
+                                }
+                            )
+
+                            res.cookie("jwt", token)
+                            res.json(token)
+                        } else {
+                            return res.sendStatus(400)
+                        }
+                    })
+                }else{
+                    return res.sendStatus(400)
+                }
+            }).catch(e=>{
+            return res.sendStatus(500)
+        })
+    }catch (e){
+        return res.sendStatus(500)
+    }
+
+}
+const verify  = (req, res) => {
+    const bearerHeader = req.headers['authorization']
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ')
+        const bearerToken = bearer[1]
+        req.token = bearerToken
+    }
+    jwt.verify(req.token, process.env.JWT_SECRET, (err, decodedUser) => {
+        if (err || !decodedUser){
+            return res.status(401).json({status: "Unauthorized Request"})
+        }else{
+            console.log(decodedUser);
+            return res.status(200).json({status: "authorized Request"})
+        }
+    })
+}
 const test = (req, res) => {
     res.json({test:"customer"})
 }
 
 module.exports = {
-test
+    signup,
+    login,
+    verify,
+    test,
 }
--- routes/customer.js
+++ routes/customer.js
@@ -3,5 +3,8 @@ const router = express.Router();//creates a router object
 const ctrl = require('../controllers');
 
 router.get('/', ctrl.customer.test);
+router.post('/signup', ctrl.customer.signup);
+router.post('/login', ctrl.customer.login);
+router.get('/verify', ctrl.customer.verify);
 
 module.exports = router
