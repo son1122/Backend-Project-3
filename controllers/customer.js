68bf0ebc970bf4880940c50b87f4e657324f1b50
son1122 <wongsatorn.n@hotmail.com>
2023-01-01 00:29:19 +0700

    add put method for customer

--- controllers/customer.js
+++ controllers/customer.js
@@ -106,20 +106,88 @@ const test = (req, res) => {
         }
 
         jwt.verify(req.token, process.env.JWT_SECRET, (err, decodedUser) => {
+            console.log(decodedUser)
+            console.log("test")
             if (err || !decodedUser)
                 return res.status(401).json({ error: "Unauthorized Request" });
 
             req.user = decodedUser;
-            // console.log(decodedUser);
-            console.log(decodedUser.username);
-            console.log(decodedUser.id);
-            return res.json("ok")
+            User.findOne({where:{username:decodedUser.username}}).then(resu=>{
+                res.json({username: resu.username,
+                    email: resu.email,
+                    phone: resu.phone,
+                    firstname: resu.firstname,
+                    lastname: resu.lastname})
+            })
         });
 }
+const edit= (req, res) => {
+    console.log(req.body)
 
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== "undefined") {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+    }
+
+    jwt.verify(req.token, process.env.JWT_SECRET, (err, decodedUser) => {
+        if (err || !decodedUser)
+            return res.status(401).json({ error: "Unauthorized Request" });
+
+        req.user = decodedUser;
+        // console.log(decodedUser);
+        console.log(decodedUser.username);
+        console.log(decodedUser.id);
+        return res.json("ok")
+    });
+    if(req.body.password!=='') {
+            bcrypt.genSalt(10, (err, salt) => {
+                if (err) return res.status(500)
+
+                bcrypt.hash(req.body.password, salt, (err, hashedPwd) => {
+                    if (err) return res.status(500)
+                    User.update(
+                        {
+                            username: req.body.username,
+                            password: hashedPwd,
+                            email: req.body.email,
+                            phone: req.body.phone,
+                            firstname: req.body.firstname,
+                            lastname: req.body.lastname
+                        },
+                        {
+                            where: {username: req.user.username},
+
+                        }
+                    ).then(r => {
+                        console.log(r);
+                    });
+                })})
+
+    }else {
+        User.update(
+            {
+                username: req.body.username,
+                email: req.body.email,
+                phone: req.body.phone,
+                firstname: req.body.firstname,
+                lastname: req.body.lastname
+            },
+            {
+                where: {username: req.user.username},
+
+            }
+        ).then(r => {
+            console.log(r);
+        });
+    }
+
+}
 module.exports = {
     signup,
     login,
     verify,
     test,
+    edit,
 }
--- routes/customer.js
+++ routes/customer.js
@@ -6,5 +6,6 @@ router.get('/', ctrl.customer.test);
 router.post('/signup', ctrl.customer.signup);
 router.post('/login', ctrl.customer.login);
 router.get('/verify', ctrl.customer.verify);
+router.put('/edit', ctrl.customer.edit);
 
 module.exports = router
