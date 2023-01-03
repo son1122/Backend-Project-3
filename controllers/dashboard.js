dbdc435f12bd4d853736dfd3bf455fbd626cccf2
son1122 <wongsatorn.n@hotmail.com>
2023-01-03 08:55:12 +0700

    add API for dash board

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,6 +2,11 @@ const {where} = require("sequelize");
 const MenuItemIngredient = require("../models").MenuItemIngredient;
 const MenuItem= require("../models").MenuItem;
 const Ingredient = require("../models").Ingredient;
+const Customer = require("../models").Customer
+const Chef = require("../models").Chef
+const Waiter = require("../models").Waiter
+const Order = require("../models").Order
+const Seller = require("../models").Seller
 const testDashboard = (req, res) => {
     res.json("test_dashboard")
 }
@@ -9,7 +14,7 @@ const getMenuDashboard = async (req, res) => {
     console.log("TEST1")
     try {
         await MenuItem.findAll( {
-            // where:{id:1},
+            where:{id:req.params.id},
             include: [
                 { model: Ingredient,
                     // attributes:['name','id','unit'],
@@ -43,9 +48,113 @@ const getMenu= async (req, res) => {
         res.status(500).send({ message: "Order not found." });
     }
 };
+const getCustomer= async (req, res) => {
+    console.log("TEST1")
+    try {
+        await Customer.findAll( {
+            attributes: ['id','firstname','lastname','phone','email'],
+
+        })
+            .then(fruit => {
+                console.log("menu get")
+                res.json(fruit)
+            })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ message: "Order not found." });
+    }
+};
+const getWaiter= async (req, res) => {
+    console.log("TEST1")
+    try {
+        await Waiter.findAll( {
+            // attributes: ['id','firstname','lastname','phone','email'],
+
+        })
+            .then(fruit => {
+                console.log("menu get")
+                res.json(fruit)
+            })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ message: "Order not found." });
+    }
+};
+const getChef= async (req, res) => {
+    console.log("TEST1")
+    try {
+        await Chef.findAll( {
+            // attributes: ['id','firstname','lastname','phone','email'],
+
+        })
+            .then(fruit => {
+                console.log("menu get")
+                res.json(fruit)
+            })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ message: "Order not found." });
+    }
+};
+const getOrder= async (req, res) => {
+    console.log("TEST1")
+    try {
+        await Order.findAll( {
+            // attributes: ['id','firstname','lastname','phone','email'],
+            include: [
+                { model: Customer,
+                    // attributes:['name','id','unit'],
+                }
+            ],
+        })
+            .then(fruit => {
+                console.log("menu get")
+                res.json(fruit)
+            })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ message: "Order not found." });
+    }
+};
+const getIngredient= async (req, res) => {
+    console.log("TEST1")
+    try {
+        await Ingredient.findAll( {
+
+        })
+            .then(fruit => {
+                console.log("menu get")
+                res.json(fruit)
+            })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ message: "Order not found." });
+    }
+};
+const getSeller= async (req, res) => {
+    console.log("TEST1")
+    try {
+        await Seller.findAll( {
+
+        })
+            .then(fruit => {
+                console.log("menu get")
+                res.json(fruit)
+            })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ message: "Order not found." });
+    }
+};
 module.exports = {
 getMenuDashboard,
-    getMenu
+    getMenu,
+    getCustomer,
+    getChef,
+    getWaiter,
+    getOrder,
+    getIngredient,
+    getSeller,
 
 
 }
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,5 +4,12 @@ const ctrl = require('../controllers');
 
 router.get('/menu/:id', ctrl.dashboard.getMenuDashboard);
 router.get('/menuitem', ctrl.dashboard.getMenu);
+router.get('/customer', ctrl.dashboard.getCustomer);
+router.get('/waiter', ctrl.dashboard.getChef);
+router.get('/chef', ctrl.dashboard.getWaiter);
+router.get('/order', ctrl.dashboard.getOrder);
+router.get('/ingredient', ctrl.dashboard.getIngredient);
+router.get('/location', ctrl.dashboard.getMenu);
+router.get('/seller', ctrl.dashboard.getSeller);
 
 module.exports = router
